a6079d65588a74e8ed41b913c89b117400ee1a46
Mauro-1998 <63267942+Mauro-1998@users.noreply.github.com>
2022-03-12 18:31:58 -0300

    Update arima.py
    Generamos la predicción a futuro, en este caso, 2 meses.

--- code/arima.py
+++ code/arima.py
@@ -40,6 +40,44 @@ for i in range(0,n_test_obser):
     actual_test_value = test_data[i]
     training_data.append(actual_test_value)
 
+# PRUEBA DE PREDICCION
+
+# Predecimos los datos futuros hasta 1 dia, tomando las predicciones como "hechos" y re-entrenando al modelo en cada iteracion
+future_predictions = []
+
+train = list(df[:]['Adj Close'])
+for i in range(0,60):
+    model = ARIMA(train, order = (4,1,0))
+    model_fit = model.fit()
+    output = model_fit.forecast()
+    yhat = output[0]
+    future_predictions.append(yhat)
+    train.append(yhat)
+    #print(train[-1])
+    #input("PAUSE")
+
+# Generamos un dataframe con fechas futuras
+# Obtenemos la ultima fecha del dataset para generar un nuevo dataset con las fechas futuras a predecir, en este caso, 60 dias en el futuro
+
+inicio = df.index[-1].date() + timedelta(1)
+fin = inicio + timedelta(60)
+
+future = pd.DataFrame()
+future['Date'] = [inicio + timedelta(days=d) for d in range((fin - inicio).days)]
+future['Adj Close'] = list(future_predictions)
+future.set_index('Date',inplace = True)
+
+df.drop(['Open'], axis=1)
+df.drop(['High'], axis=1)
+df.drop(['Low'], axis=1)
+df.drop(['Close'], axis=1)
+df.drop(['Volume'], axis=1)
+
+# Unimos los dataframes por columnas
+pd.concat([df, future], axis=0) 
+
+# FINAL PRUEBA PREDICCION
+
 
 print(model_fit.summary())
 plt.figure(figsize=(15,9))
